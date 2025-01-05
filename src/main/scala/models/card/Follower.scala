44d8e8fc46a2d2a78fcb281d6b8b0c14bd5523fb
iroha168 <kimura.masatoshi74@gmail.com>
2025-01-05 15:16:15 +0900

    fix follower create methods

--- src/main/scala/models/card/Follower.scala
+++ src/main/scala/models/card/Follower.scala
@@ -6,12 +6,25 @@ case class Follower(
                      name: CardName,
                      cardType: CardType,
                      cost: Cost,
-                     attack: Option[Attack],
-                     health: Option[FollowerHealth],
+                     attack: Attack,
+                     health: FollowerHealth,
                      abilities: Option[String]
                    ) extends Card(name, CardType.Follower, cost, abilities)
 
 object Follower:
+  def unsafeCreate(
+                    safeName: String,
+                    safeCost: Int,
+                    safeAttack: Int,
+                    safeHealth: Int,
+                    safeAbilities: Option[String]
+                  ): Follower =
+    val name = CardName.applyUnsafe(safeName)
+    val cost = Cost.applyUnsafe(safeAttack)
+    val attack = Attack.applyUnsafe(safeHealth)
+    val health = FollowerHealth.applyUnsafe(safeHealth)
+    Follower(name, CardType.Follower, cost, attack, health, safeAbilities)
+
   def create(
               name: String,
               cost: Int,
@@ -24,5 +37,5 @@ object Follower:
       validCost <- Cost.either(cost)
       validAttack <- Attack.either(attack)
       validHealth <- FollowerHealth.either(health)
-    yield Follower(validName, CardType.Follower, validCost, Some(validAttack), Some(validHealth), abilities)
+    yield Follower(validName, CardType.Follower, validCost, validAttack, validHealth, abilities)
 
